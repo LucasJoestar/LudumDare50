ae28bddbe3279552c905adf7da4938655a8293e5
Alexis <thiebautalexis01@gmail.com>
2022-04-04 23:26:58 +0200

    Modif Patterns & Fade Musics

--- Assets/200 Technical/Sounds/SoundManager.cs
+++ Assets/200 Technical/Sounds/SoundManager.cs
@@ -17,8 +17,14 @@ namespace LudumDare50
         [Section("SoundManager")]
         [SerializeField] private AudioMixer mixer = null;
         [SerializeField] private AudioSource sourcePrefab = null;
+        [SerializeField] private AudioSource sourceMusic = null;
+        [SerializeField] private AudioClip musicMainMenu = null;
+        [SerializeField] private AudioClip musicInGame = null;
+        [SerializeField, Enhanced, Range(.1f, 1f)] private float fadeInDuration = 1.0f;
+        [SerializeField, Enhanced, Range(.1f, 1f)] private float fadeOutDuration = 1.0f;
 
         private Queue<AudioSource> sources = new Queue<AudioSource>();
+        private Sequence sequence = null;
         #endregion
 
         public void PlayClip(AudioClip _clip, float _volumeScale = 1.0f)
@@ -42,5 +48,32 @@ namespace LudumDare50
             float _dB = Mathf.Lerp(-30f, 0f, _value);
             mixer.SetFloat("Volume", _dB);
         }
+
+        [Button]
+        public void SwitchMusicTo(MusicType _type)
+        {
+            if (sequence.IsActive()) sequence.Kill(true);
+            sequence = DOTween.Sequence();
+            sequence.Join(sourceMusic.DOFade(0, fadeOutDuration));
+            switch (_type)
+            {
+                case MusicType.Menu:
+                    sequence.AppendCallback(() => sourceMusic.clip = musicMainMenu);
+                    break;
+                case MusicType.InGame:
+                    sequence.AppendCallback(() => sourceMusic.clip = musicInGame);
+                    break;
+            }
+            sequence.Append(sourceMusic.DOFade(1, fadeInDuration));
+            sequence.AppendCallback(sourceMusic.Play);
+            sequence.Play();
+        }
+
+    }
+
+    public enum MusicType
+    {
+        Menu, 
+        InGame
     }
 }
